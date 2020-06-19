f25f567fa5fa6149c425396a5f4924a729bb00d4
Abderrahmane BAYANE <abdrahmanbayan@gmail.com>
2020-06-19 23:20:20 +0200

    My initial commit

---
+++ BankAccountKata.Tests/AccountTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankAccountKata.Tests
+{
+    [TestClass]
+    public class AccountTests
+    {
+        [TestMethod]
+        public void Should_Have_0_As_Balance_When_Account_Is_Newly_Created()
+        {
+            decimal expectedBalance = 0;
+            Account account = new Account();
+
+            decimal actualBalance = account.Balance;
+
+            Assert.AreEqual(expectedBalance, actualBalance);
+        }
+    }
+}
---
+++ BankAccountKata.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankAccountKata.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankAccountKata.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2cde2fd2-d8e8-498a-93df-896dee18112f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
